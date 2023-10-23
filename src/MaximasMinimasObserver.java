aeb0e9568ec72824e755d30152c0512fc585dad3
Kevin Depolli <kevindepollidcf@gmail.com>
2023-10-23 17:01:32 -0300

    Implementar Método Exibir Máxima e Mínima

--- src/MaximasMinimasObserver.java
+++ src/MaximasMinimasObserver.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MaximasMinimasObserver implements Painel {
 
@@ -14,7 +13,30 @@ public class MaximasMinimasObserver implements Painel {
         exibirMaximasMinimas();
     }
     private void exibirMaximasMinimas() {
-        //Obtem máximas e mínimas
+        DadoClima maxTemperatura, minTemperatura;
+        DadoClima maxUmidade, minUmidade;
+        DadoClima maxPressao, minPressao;
+
+        maxTemperatura = Collections.max(dadosClima, Comparator.comparing(DadoClima::getTemperatura));
+        minTemperatura = Collections.min(dadosClima, Comparator.comparing(DadoClima::getTemperatura));
+
+        maxUmidade = Collections.max(dadosClima, Comparator.comparing(DadoClima::getUmidade));
+        minUmidade = Collections.min(dadosClima, Comparator.comparing(DadoClima::getUmidade));
+
+        maxPressao = Collections.max(dadosClima, Comparator.comparing(DadoClima::getPressao));
+        minPressao = Collections.min(dadosClima, Comparator.comparing(DadoClima::getPressao));
+
+        System.out.println("A máxima e a mínima temperatura registrada foram de "
+                + maxTemperatura.getTemperatura() + " em " + maxTemperatura.getData()
+        + " e " + minTemperatura.getTemperatura() + " em " + minTemperatura.getData()
+        + "\nA máxima e a mínima Umidade registrada foram de "
+                + maxUmidade.getUmidade() + " em " + maxUmidade.getData()
+        + " e " + minUmidade.getUmidade() + " em " + minUmidade.getData()
+        + "\nA máxima e a mínima Pressao registrada foram de "
+                + maxPressao.getPressao() + " em " + maxPressao.getData()
+        + " e " + minPressao.getPressao() + " em " + minPressao.getData()
+        + "\n");
+
     }
 }
 
